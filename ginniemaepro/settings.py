13def1d1771d2780b8a4bd40814789cfd7311f5d
manucodesvera <manukrishna@codesvera.com>
2023-11-29 17:25:14 +0530

    removed loader and added download link

--- ginniemaepro/settings.py
+++ ginniemaepro/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-9m170=x_+%$)n1247%vrt$xr&=woazr(kce(esm$@&h3^0!bg6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
